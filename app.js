21b5505196077b17d258455a4bf4c8ad9835808a
DonPrus <donprusne@gmail.com>
2017-07-30 16:19:27 +0300

    Small refactoring, add some style

--- app.js
+++ app.js
@@ -4,24 +4,45 @@ Vue.component('example-component-name', {
 
 Vue.component('todo-item', {
     props: ['todo'],
-    template: '<li>{{ todo.text }}</li>'
+    computed: {
+        getTextClass: function () {
+            return 'has-text-' + this.todo.color;
+        }
+    },
+    template: '<li class="is-size-5"><span v-bind:class="getTextClass">{{ todo.text }}</span></li>'
 });
+
+const DEFAULT_COLOR = 'primary';
 
 let app = new Vue({
     el: '#app',
     data: {
         message: 'Hello Vue!',
+        colors: [
+            'primary',
+            'info',
+            'success',
+            'warning',
+            'danger'
+        ],
+        selectedColor: DEFAULT_COLOR,
         itemList: [
-            {text: 'Learn HTML & CSS'},
-            {text: 'Learn JavaScript'},
-            {text: 'Learn VueJS'},
-            {text: 'Build something amazing'},
-        ]
+            {id: 1, text: 'Learn HTML & CSS', color: 'primary'},
+            {id: 2, text: 'Learn JavaScript', color: 'primary'},
+            {id: 3, text: 'Learn VueJS', color: 'primary'},
+            {id: 4, text: 'Build something amazing', color: 'info'},
+        ],
     },
     methods: {
         add: function (event) {
-            this.itemList.push({ text: this.message });
+            if (this.message.trim() !== '') {
+                this.itemList.push({
+                    text: this.message,
+                    color: this.selectedColor,
+                });
+            }
             this.message = '';
+            this.selectedColor = DEFAULT_COLOR;
         }
     }
 });
